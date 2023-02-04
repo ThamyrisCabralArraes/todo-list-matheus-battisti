dee09f592fe98ee215e4c036479e9cc088ad6778
thamyris <thamyriscabral@hotmail.com>
2023-02-04 10:33:26 -0300

    adicionei tags

--- src/App.js
+++ src/App.js
@@ -6,9 +6,45 @@ import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons/bs';
 const API = 'http://localhost:5000';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [time, setTime] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [loading, setloading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('enviou');
+  };
+
   return (
     <div className='App'>
       <h1>Todo</h1>
+      <div className='todo-header'>
+        <h1>Todo List React</h1>
+      </div>
+      <div className='form-todo'>
+        <h2>Lista</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='title'>Tafera:</label>
+            <input
+              type='text'
+              name='title'
+              placeholder='Digite aqui sua tarefa'
+              required
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <input
+            type='submit'
+            value='Enviou'
+          />
+        </form>
+      </div>
+      <div className='list-todo'>
+        <p>Lista de Tarefas:</p>
+        {todos.length === 0 && <p>Não há tarefas.</p>}
+      </div>
     </div>
   );
 }
