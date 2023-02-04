6fc938f53dacd970f603ebaf92638c09431ee901
thamyris <thamyriscabral@hotmail.com>
2023-02-04 11:28:39 -0300

    introduzi tarefas na lista e criei botao remove

--- src/App.js
+++ src/App.js
@@ -13,37 +13,52 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('enviou');
+  };
+
+  const handletaskTheList = () => {
+    setTitle('');
+    setTodos([...todos, title]);
   };
 
   return (
     <div className='App'>
-      <h1>Todo</h1>
       <div className='todo-header'>
         <h1>Todo List React</h1>
       </div>
       <div className='form-todo'>
-        <h2>Lista</h2>
+        <h1>Lista</h1>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor='title'>Tafera:</label>
+            <label htmlFor='title'>Tafera: </label>
             <input
               type='text'
               name='title'
               placeholder='Digite aqui sua tarefa'
+              value={title || ''}
               required
               onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <input
+            className='input-enviar'
             type='submit'
-            value='Enviou'
+            value='Enviar'
+            onClick={handletaskTheList}
           />
         </form>
       </div>
       <div className='list-todo'>
-        <p>Lista de Tarefas:</p>
-        {todos.length === 0 && <p>Não há tarefas.</p>}
+        <h1>Lista de Tarefas:</h1>
+        <div className='div-tarefas'>
+          {todos.length === 0 && <p>Não há tarefas.</p>}
+          {todos.length !== 0 &&
+            todos.map((todo) => (
+              <div className='div-container'>
+                <h2 key={todo}>{todo} </h2>
+                <button className='button-remove'>x</button>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
